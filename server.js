3c077212fc9d93b37abe93be5dc0a99e4750c09e
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-03-17 10:28:15 -0600

    add middleware

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // Imports
 import express from 'express';
+import mongoose from 'mongoose';
+import ChatRoom from './model/db.chatRooms.js';
 import Pusher from "pusher";
+import cors from 'cors';
+import path from 'path';
 
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
@@ -18,3 +22,11 @@ const pusher = new Pusher({
     cluster: process.env.SERVER_PUSHER_CLUSTER,
     encrypted: true
 });
+
+
+// Middleware
+app.use(express.json());
+app.use(express.static('whatsapp-frontend/build'));
+
+/*Cross-Origin Resource Sharing*/
+app.use(cors());
